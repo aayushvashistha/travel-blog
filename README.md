02ec3f3c93aaca099333fb1e029b030779c48409
Aayush Vashistha <aayush.vashistha@gmail.com>
2023-05-22 20:11:51 -0400

    created mongodb connection

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # travel-blog
 Creating travel blog website using JS, jinja templating and Django(backend) 
+
+
+To run a server we must use -
+
+> python .\manage.py runserver
--- myapp/views.py
+++ myapp/views.py
@@ -1,11 +1,14 @@
+import sys
+sys.path.append("..")
 from django.shortcuts import render
+from utils import get_db_handle
 #from django.http import HttpResponse
 
 # Create your views here.
 
 context ={
         "data":"Contact Me",
-        #"list":[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        "list":[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     }
 
 def index(request):
@@ -17,5 +20,9 @@ def about(request):
     return render(request, 'about.html')
 
 def contact(request):
-    
+    print(get_db_handle('127.0.0.1', 27017, context))
+    print('Inside Contact')
+    #collection.insert_one(context)
     return render(request, 'contact.html', context)
+
+# get_db_handle('127.0.0.1', 27017)
