599aa59200cbc4ac46c0fc668ee515afaf2c3535
Aayush Vashistha <aayush.vashistha@gmail.com>
2023-06-03 15:18:48 -0400

    Models included

--- myproject/urls.py
+++ myproject/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    #path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', include('myapp.urls')),
     #path('about/', include('myapp.urls'))
 ]
